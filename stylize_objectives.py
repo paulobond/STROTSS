ba95a3aae5db7edfd14e4c0b00dd3df9b215dbd9
Paul Bonduelle <paul@feedly.com>
2020-01-19 12:12:39 +0100

    wip

--- stylize_objectives.py
+++ stylize_objectives.py
@@ -80,6 +80,7 @@ class objective_class():
 
             ### Combine Terms and Normalize ###
             ell_style = remd_loss +moment_weight*moment_ell
+            ell_style = moment_weight*moment_ell
             style_weight = 1.0 + moment_weight
             final_loss += (content_weight*ell_content+ell_style)/(content_weight+style_weight)
             # final_loss = ell_style
